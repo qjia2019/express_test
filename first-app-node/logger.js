30d339a1157b4667d566e0f715abbc896e88757d
qjia2019 <qjia2019@gmail.com>
2021-08-14 20:28:56 -0400

    first commit

---
+++ first-app-node/logger.js
@@ -0,0 +1,9 @@
+console.log(__filename);
+console.log(__dirname);
+function log(message) {
+  console.log(message);
+}
+let url = "http://www.baidu.com";
+
+//module.exports.fraud = url;
+module.exports = log;
